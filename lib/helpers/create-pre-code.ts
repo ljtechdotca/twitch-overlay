87b710860d05e64fa182eac40e89a065a3a3cdd0
Landon Johnson <59711105+ljtechdotca@users.noreply.github.com>
2022-04-01 04:36:24 -0600

    [feat] format code blocks

--- lib/helpers/create-pre-code.ts
+++ lib/helpers/create-pre-code.ts
@@ -1,32 +1,29 @@
 import hljs from "highlight.js";
+import prettier from "prettier";
+import babel from "prettier/parser-babel";
+import html from "prettier/parser-html";
+import postcss from "prettier/parser-postcss";
+import typescript from "prettier/parser-typescript";
 
 export const createPreCode = (message: string) => {
   const pre = document.createElement("pre");
   pre.classList.add("pre_code");
 
-  const supportedLangs = [
-    "cs",
-    "css",
-    "go",
-    "html",
-    "js",
-    "json",
-    "jsx",
-    "md",
-    "py",
-    "sass",
-    "scss",
-    "ts",
-    "tsx",
-    "vue",
-    "xaml",
-    "xml",
-    "php",
-  ];
+  const supportedLangs = ["cs", "css", "html", "js", "sass", "scss", "ts"];
+  const supportedParsers = ["css", "html", "js", "sass", "scss", "ts"];
+  const parsers: Record<string, string> = {
+    css: "css",
+    sass: "css",
+    scss: "css",
+    js: "babel",
+    html: "html",
+    ts: "typescript",
+  };
+
   const code = document.createElement("code");
 
   const matchCode = message.match(`\`((.|/n)*)\``);
-  
+
   if (matchCode) {
     const codeFromMessage = matchCode[1];
     const codeSplit = codeFromMessage.split(" ");
@@ -34,9 +31,20 @@ export const createPreCode = (message: string) => {
     const codeBlock = codeSplit.slice(1).join(" ");
 
     if (supportedLangs.includes(codeLang)) {
-      const highlight = hljs.highlight(codeBlock, {
-        language: codeLang,
-      }).value;
+      let formatCodeBlock: string | null = null;
+      if (supportedParsers.includes(codeLang)) {
+        formatCodeBlock = prettier.format(codeBlock, {
+          parser: parsers[codeLang],
+          plugins: [babel, postcss, html, typescript],
+        });
+      }
+
+      const highlight = hljs.highlight(
+        formatCodeBlock ? formatCodeBlock : codeBlock,
+        {
+          language: codeLang,
+        }
+      ).value;
       code.innerHTML = highlight;
     } else {
       if (codeBlock.length > 100) {
